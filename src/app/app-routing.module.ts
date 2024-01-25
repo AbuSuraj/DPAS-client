4c8642641ae406351cf9ef4a1f9337bfa06efc03
Abu Suraj <abusurajbd@gmail.com>
2024-01-25 19:02:48 +0600

    add dashboard module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardModule } from './dashboard/dashboard.module';
 
-const routes: Routes = [ {
+const routes: Routes = [ 
+  {
   path: 'account',
   loadChildren: () => import('./account/account.module').then((m) => m.AccountModule),
-}];
+},
+{
+  path:'dashoard',
+  loadChildren:() => import('./dashboard/dashboard.module').then((m)=>m.DashboardModule)
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
