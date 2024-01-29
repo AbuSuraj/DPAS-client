3ba5e246906f19259c042321452c7693718221b8
Abu Suraj <abusurajbd@gmail.com>
2024-01-29 19:38:16 +0600

    add login into localstorage to save its state

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -4,12 +4,17 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthenticationService {
-  private loggedInSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  loggedIn$: Observable<boolean> = this.loggedInSubject.asObservable();
+  private loggedInSubject: BehaviorSubject<boolean>;
+  loggedIn$: Observable<boolean>;
   
-  constructor() { }
+  constructor() { 
+    const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+    this.loggedInSubject = new BehaviorSubject<boolean>(isLoggedIn);
+    this.loggedIn$ = this.loggedInSubject.asObservable();
+  }
   
   setLoggedIn(value: boolean): void {
     this.loggedInSubject.next(value);
+    localStorage.setItem('isLoggedIn', value ? 'true' : 'false');
   }
 }
